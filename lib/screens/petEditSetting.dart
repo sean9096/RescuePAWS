97ee6499789ac64301a6709bdb098ba7cd139a10
Alyissa Sanders <32529396+Ajalley1828@users.noreply.github.com>
2021-08-01 13:29:22 -0500

    Pet edit setting section of settings

--- lib/screens/petEditSetting.dart
+++ lib/screens/petEditSetting.dart
@@ -43,20 +43,65 @@ class _PetEditSettingPageState extends State<PetEditSettingPage> {
           child: ListView(
             children: [
               Text(
-                "Pet Edit Profile",
+                "Edit Pet Profile",
                 style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
               ),
               SizedBox(
                 height: 15,
               ),
-              Center(child: Stack()),
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 4,
+                              color: Theme.of(context).scaffoldBackgroundColor),
+                          boxShadow: [
+                            BoxShadow(
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                color: Colors.black.withOpacity(0.1),
+                                offset: Offset(0, 10))
+                          ],
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                "https://images.pexels.com/photos/3307758/pexels-photo-3307758.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250",
+                              ))),
+                    ),
+                    Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 4,
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                            ),
+                            color: Colors.purple,
+                          ),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                        )),
+                  ],
+                ),
+              ),
               SizedBox(
                 height: 35,
               ),
-              buildTextField("Full Name", "Dor Alex", false),
-              buildTextField("E-mail", "alexd@gmail.com", false),
-              buildTextField("Password", "********", true),
-              buildTextField("Location", "TLV, Israel", false),
+              buildTextField("Pet Name", "winter", false),
+              buildTextField("Pet Age", "5 months", false),
+              buildTextField("Contact Number", "6512743566", false),
+              buildTextField("Contact Email", "Email@gmail.com", false),
               SizedBox(
                 height: 35,
               ),
